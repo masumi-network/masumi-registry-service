4e1a3d712cc875291499d6f1b97fcf241bed535d
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2026-04-13 07:04:25 +0200

    fix: jest

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+};
